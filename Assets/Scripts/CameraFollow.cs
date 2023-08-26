94180ecf74c667052431419e0cb5e8a1045e43ce
João Felipe <jfk.abrao@hotmail.com>
2023-08-25 22:34:27 -0300

    v1.1

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,20 +4,52 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+
+    public Transform cameraBounds;
+    private Camera mainCamera;
+
+    private float maxY;
+
     [SerializeField] public float FollowSpeed = 2f;
     [SerializeField] public float yOffset = 1f;
     [SerializeField] public float xOffset = 1f;
     [SerializeField] public Transform target;
+
+    private float Xtot;
+    private float Ytot;
     // Start is called before the first frame update
     void Start()
     {
-
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x + xOffset, target.position.y + yOffset, -10f);
-        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+        Xtot = target.position.x + xOffset;
+        Ytot = target.position.y + yOffset;
+        Vector3 cameraPosition = mainCamera.transform.position;
+        maxY = Mathf.Clamp(cameraPosition.y, cameraBounds.position.y, Mathf.Infinity);
+        
+        
+        if (Ytot > maxY){
+            Ytot = maxY;
+            Vector3 newPos = new Vector3(Xtot, maxY, -10f);
+            transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+            print("maximo");
+        } else
+        {
+            Vector3 newPos = new Vector3(Xtot, Ytot, -10f);
+            transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+        }
+        
+        print(maxY);
+    }
+    private void LateUpdate()
+    {
+        
+        
+        //mainCamera.transform.position = cameraPosition;
+
     }
 }
