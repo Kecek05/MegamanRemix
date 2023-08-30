956c45678b69e3649c990a23c322092f8b17a0a3
Yan Hideki Yashiki <yan.yashiki@gmail.com>
2023-08-30 10:29:16 -0300

    v1
    Inicio fisica do tiro

--- Assets/Scripts/EsqueletoBullet.cs
+++ Assets/Scripts/EsqueletoBullet.cs
@@ -2,22 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EsqueletoBullet : MonoBehaviour
+
+public class EsqueletoBullet: MonoBehaviour
+
 {
-    public float initialSpeed = 5.0f; // Velocidade inicial do tiro do inimigo
-    private Rigidbody2D rb;
 
-    private void Start()
+    public GameObject TiroEsqueletoPrefab; // Prefab do projétil
+
+    public Transform EsqueletoTiroSpawn;         // Ponto de onde o projétil será disparado
+
+    public float fireForce = 10f;       // Força do disparo
+
+    public float angle = 45f;           // Ângulo de lançamento
+
+    public float fireRate = 2f;         // Taxa de disparo em segundos
+
+
+
+    private float nextFireTime;
+
+
+
+    void Start()
+
     {
-        rb = GetComponent<Rigidbody2D>();
 
-        // Inicialize a velocidade inicial do tiro
-        rb.velocity = transform.right * initialSpeed;
+        nextFireTime = Time.time + fireRate;
+
     }
 
-    // Update is called once per frame
+
+
     void Update()
+
     {
-        
+
+        if (Time.time >= nextFireTime)
+
+        {
+
+            FireProjectile();
+
+            nextFireTime = Time.time + fireRate;
+
+        }
+
     }
+
+
+
+    void FireProjectile()
+
+    {
+
+        GameObject newProjectile = Instantiate(TiroEsqueletoPrefab, EsqueletoTiroSpawn.position, Quaternion.identity);
+
+        Rigidbody2D rb = newProjectile.GetComponent<Rigidbody2D>();
+
+
+
+        // Converter ângulo para radianos
+
+        float angleRad = angle * Mathf.Deg2Rad;
+
+
+
+        // Calcular componentes x e y da velocidade inicial
+
+        float initialVelocityX = fireForce * Mathf.Cos(angleRad);
+
+        float initialVelocityY = fireForce * Mathf.Sin(angleRad);
+
+
+
+        // Definir a velocidade inicial do projétil
+
+        rb.velocity = new Vector2(initialVelocityX, initialVelocityY);
+
+    }
+
 }
